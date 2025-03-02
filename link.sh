41acce867d0005f0de427e66b810d05ec08f6cb6
TideS <tidesmain@gmail.com>
2025-03-02 16:17:11 +0500

    Add link.sh

---
+++ link.sh
@@ -0,0 +1,30 @@
+CONFIG_FOLDERS=(
+    "nvim/"
+    "alacritty/"
+)
+
+for folder in "${CONFIG_FOLDERS[@]}";do
+    rm -rf "${HOME}/.config/${folder}"
+    cp -lR "${folder}" "${HOME}/.config/"
+done
+
+HOME_FOLDERS=(
+    ".emacs.d/"
+)
+
+for folder in "${HOME_FOLDERS[@]}";do
+    rm -rf "${HOME}/${folder}"
+    cp -lR "${folder}" "${HOME}/"
+done
+
+
+HOME_FILES=(
+    ".fakevimrc"
+    ".ideavimrc"
+    ".zshrc"
+)
+
+for file in "${HOME_FILES[@]}";do
+    rm -rf "${HOME}/${file}"
+    cp -lR "${file}" "${HOME}/${file}"
+done
