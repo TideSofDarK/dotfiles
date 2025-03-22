6dcb05bf67ccc0738889c8af0128610fe9c1cb46
TideS <tidesmain@gmail.com>
2025-03-22 23:57:48 +0500

    Whatever

--- nvim/lua/user/plugins/tree.lua
+++ nvim/lua/user/plugins/tree.lua
@@ -77,6 +77,10 @@ return {
         },
       },
     })
+    local function close_nvim_tree()
+      require("nvim-tree.api").tree.close()
+    end
+    vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = close_nvim_tree })
   end,
   keys = {
     {
