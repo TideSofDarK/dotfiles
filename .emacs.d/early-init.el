751845442eed856521bcdf750e3342aedf95a0ec
TideS <tidesmain@gmail.com>
2025-05-13 12:28:29 +0500

    Whatever

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -590,7 +590,7 @@
            :inherit nil
            :after treesit)
   :custom
-  (treesit-langs-bundle-version "0.12.276")
+  (treesit-langs-bundle-version "0.12.277")
   :init
   (advice-add 'treesit-langs-install-grammars :around #'suppress-messages)
   :config
