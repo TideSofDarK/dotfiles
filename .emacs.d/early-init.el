a19435cba53bb28b48057ac2ec08e3abcf35eb78
TideS <tidesmain@gmail.com>
2025-05-15 12:46:14 +0500

    Whatever

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -29,7 +29,7 @@
 ;;; Optimize GC
 
 (setq gc-cons-threshold (eval-when-compile (* 1024 1024 1024)))
-(run-with-idle-timer 2 t (lambda () (garbage-collect)))
+(run-with-idle-timer 2 t 'garbage-collect)
 
 ;;; Maximize On Launch
 
