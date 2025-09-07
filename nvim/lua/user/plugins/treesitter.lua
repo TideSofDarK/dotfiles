3a3bdfeca972f9574853e0af0171d05193e21207
TideS <tidesmain@gmail.com>
2025-09-07 13:00:34 +0500

    Whatever

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,8 +1,13 @@
 vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' } })
 
 require('nvim-treesitter').install({ 'c', 'cpp', 'lua', 'python', 'vimdoc', 'vim', 'bash', 'glsl', 'markdown',
-  'markdown_inline', 'yaml', 'gdscript', 'rust' }):wait(300000)
+  'markdown_inline', 'yaml', 'gdscript', 'rust' })
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
-  callback = function() vim.treesitter.start() end,
+  callback = function(ctx)
+    local has_started = pcall(vim.treesitter.start)
+    local no_indent = {}
+    if has_started and not vim.list_contains(no_indent, ctx.match) then
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+  end,
 })
