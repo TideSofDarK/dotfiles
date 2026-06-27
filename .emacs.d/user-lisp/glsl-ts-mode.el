ff1371cb6930c72defe40fc17794778de60ff716
TideS <tidesmain@gmail.com>
2026-06-27 11:12:20 +0500

    Whatever

--- .emacs.d/user-lisp/glsl-ts-mode.el
+++ .emacs.d/user-lisp/glsl-ts-mode.el
@@ -194,13 +194,13 @@
      "invariant" "lowp" "mediump" "highp")
   "GLSL qualifiers for tree-sitter font-locking.")
 
-(defvar glsl-ts--layout-qualifiers
+(defvar glsl-ts--image-layout-qualifiers
   '("rgba32f" "rgba16f" "rg32f" "rg16f" "r11f_g11f_b10f" "r32f" "r16f" "rgba16"
      "rgb10_a2" "rgba8" "rg16" "rg8" "r16" "r8" "rgba16_snorm" "rgba8_snorm"
      "rg16_snorm" "rg8_snorm" "r16_snorm" "r8_snorm" "rgba32i" "rgba16i" "rgba8i"
      "rg32i" "rg16i" "rg8i" "r32i" "r16i" "r8i" "rgba32ui" "rgba16ui" "rgb10_a2ui"
      "rgba8ui" "rg32ui" "rg16ui" "rg8ui" "r32ui" "r16ui""r8ui")
-  "GLSL layout qualifiers for tree-sitter font-locking.")
+  "GLSL image layout qualifiers for tree-sitter font-locking.")
 
 (defvar glsl-ts--font-lock-settings
   (treesit-font-lock-rules
@@ -287,7 +287,7 @@
     :feature 'qualifier
     `(((layout_qualifiers
          (qualifier (identifier) @font-lock-type-face
-           (:match ,(rx-to-string `(seq bol (or ,@glsl-ts--layout-qualifiers) eol)) @font-lock-type-face))))
+           (:match ,(rx-to-string `(seq bol (or ,@glsl-ts--image-layout-qualifiers) eol)) @font-lock-type-face))))
        ((type_qualifier) @font-lock-keyword-face))
 
     :language 'glsl
